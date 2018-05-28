302db2225e44b4b7887398a13afa156e309752e1
vikingfacer <jacobmontpetit@gmail.com>
2018-05-28 18:58:41 -0400

    still in a boot loop at the end of post remaping the kernel

--- src/lib.rs
+++ src/lib.rs
@@ -28,27 +28,13 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
     let memory_map_tag = boot_info.memory_map_tag()
         .expect("Memory map tag required");
-
-    println!("memory areas:");
-    for area in memory_map_tag.memory_areas() {
-        println!("    start: 0x{:x}, length: 0x{:x}",
-            area.base_addr, area.length);
-    }
-
     let elf_section_tag = boot_info.elf_sections_tag()
         .expect("elf-section tag required");
-
-    println!("kernel sections:");
-    for section in elf_section_tag.sections() {
-        println!("   addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-            section.addr, section.size, section.flags);
-    }
 
     let kernel_start = elf_section_tag.sections().map(|s| s.addr)
         .min().unwrap();
     let kernel_end = elf_section_tag.sections().map(|s| s.addr + s.size)
         .max().unwrap();
-
     let multiboot_start = multiboot_information_address;
     let multiboot_end = multiboot_start + (boot_info.total_size as usize);
 
@@ -60,14 +46,11 @@ pub extern fn rust_main(multiboot_information_address: usize) {
         multiboot_end, memory_map_tag.memory_areas());
 
     println!("{:?}", frame_allocator.allocate_frame());
-    memory::test_paging(&mut frame_allocator);
 
-    for i in 0.. {
-        if let None = frame_allocator.allocate_frame() {
-            println!("allocated {} frames", i);
-            break;
-        }
-    }
+    enable_nxe_bit();
+    enable_write_protect_bit();
+    memory::remap_the_kernel(&mut frame_allocator, boot_info);
+    println!("It did not crash!");
 
     loop{}
 }
@@ -82,4 +65,20 @@ pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str,
     println!("\n\nPANIC IN {} at line {}:",file, line);
     println!("   {}", fmt);
     loop{}
+}
+
+fn enable_nxe_bit() {
+    use x86_64::registers::msr::{IA32_EFER, rdmsr, wrmsr};
+
+    let nxe_bit = 1 << 11;
+    unsafe {
+        let efer = rdmsr(IA32_EFER);
+        wrmsr(IA32_EFER, efer | nxe_bit);
+    }
+}
+
+fn enable_write_protect_bit() {
+    use x86_64::registers::control_regs::{cr0, cr0_write, Cr0};
+
+    unsafe { cr0_write(cr0() | Cr0::WRITE_PROTECT) };
 }
--- src/memory/paging/entry.rs
+++ src/memory/paging/entry.rs
@@ -1,4 +1,27 @@
 use memory::Frame; // needed later
+use multiboot2::ElfSection;
+
+impl EntryFlags {
+    pub fn from_elf_section_flags(section: &ElfSection) -> EntryFlags {
+        use multiboot2::{ELF_SECTION_ALLOCATED, ELF_SECTION_WRITABLE,
+            ELF_SECTION_EXECUTABLE};
+
+        let mut flags = EntryFlags::empty();
+
+        if section.flags().contains(ELF_SECTION_ALLOCATED) {
+            // section is loaded to memory
+            flags = flags | PRESENT;
+        }
+        if section.flags().contains(ELF_SECTION_WRITABLE) {
+            flags = flags | WRITABLE;
+        }
+        if !section.flags().contains(ELF_SECTION_EXECUTABLE) {
+            flags = flags | NO_EXECUTE;
+        }
+
+        flags
+    }
+}
 
 pub struct Entry(u64);
 
